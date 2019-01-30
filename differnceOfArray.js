fbc5af8bea16dde3f3581b810525cfd5acc68db5
luben <lubenfrank48@gmail.com>
2019-01-30 15:29:36 +0530

    modified

--- differnceOfArray.js
+++ differnceOfArray.js
@@ -1,15 +1,17 @@
 function difference(arr, k) {
   arr.sort((a, b) => a - b);
   var count = 0;
+  var newArr=[];
   for (var i = 0; i < arr.length - 1; i++) {
     for (var j = i; j < arr.length; j++) {
       if (arr[j] - arr[i] > k) {
         break;
       } else if (arr[j] - arr[i] == k) {
+        newArr.push([arr[j],arr[i]]);
         count++;
       }
     }
   }
-  return count;
+  return `total number of pairs with difference ${k} are ${count} and the pairs are ${newArr}`;
 }
-difference([1, 2, 3, 4,5], 1);
+difference([1, 2, 3, 4,], 1);
